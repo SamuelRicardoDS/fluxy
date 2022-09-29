2651fbb0102c30fe20f13f4562c99dd9430744d1
SamuelRicardoDS <samuelricardods@gmail.com>
2022-09-28 23:13:35 -0300

    nada

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -14,19 +14,27 @@ export const Register = () => {
     }
 
     return(
-        <div> 
-            <form onSubmit={handleSubmit} className="flex flex-col">
-                <div>
-                    <label htmlFor="">nome</label>
-                    <input id="nome" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                    <label id="sobrenome" htmlFor="">sobrenome</label>
-                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+        <div className="bg-neutral-600 p-5 h-screen w-screen"> 
+            <form onSubmit={handleSubmit} className="flex flex-col w-1/2">
+                <div className="flex">
+                    <div className="flex flex-col p-5">
+                        <label htmlFor="">nome</label>
+                        <input id="nome" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div className="flex flex-col p-5">
+                        <label id="sobrenome" htmlFor="">sobrenome</label>
+                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                    </div>
                 </div>
-                <label id="email" htmlFor="">email</label>
-                <input type="email" value={email} onChange={(e => setEmail(e.target.value))}/>
-                <label id="senha" htmlFor="">password</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit">cadastrar</button>
+                <div className="p-5 flex flex-col">
+                    <label id="email" htmlFor="">email</label>
+                    <input type="email" value={email} onChange={(e => setEmail(e.target.value))}/>
+                </div>
+                <div className="p-5 flex flex-col">
+                    <label id="senha" htmlFor="">password</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit" className="bg-slate-500">cadastrar</button>
             </form>
             <BackButton />
         </div>
