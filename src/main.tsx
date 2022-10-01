e5e05a7183298825b8d0203ec4ce9c2356d3ed1d
SamuelRicardoDS <samuelricardods@gmail.com>
2022-09-30 22:20:11 -0300

    login dura um segundo

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./contexts/auth/AuthProvider";
+import "./main.css";
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
-)
+);
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,37 @@
 import { BackButton } from "../components/BackButton";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/auth/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-  function handleSubmit() {
-  }
+  const auth = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async () => {
+    if (email && password) {
+      const isLogged = await auth.signIn(email, password);
+      if (isLogged) {
+        navigate('/home')
+      } else {
+        alert("Usuário ou senha inválidos");
+      }
+    }
+    window.location.href = "/home";
+  }  
+
   return (
     <div className="flex flex-col bg-slate-400 w-1/2 h-screen items-center">
       <form className="flex flex-col">
         <label htmlFor="">Usuário</label>
-        <input type="text" />
+        <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
         <label htmlFor="">Senha</label>
-        <input type="password" />
-        <button>login</button>
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
       </form>
+        <button onClick={() => handleSubmit()}>login</button>
       <BackButton />
     </div>
   );
